f6894041c6531ddbbb788642a1a0b372514ce0cc
Samirul Islam <samirul077islam@gmail.com>
2024-07-30 20:17:07 +0530

    3nd commit

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
 const feedRoute = require("./routes/feed");
 const app = express();
 
 app.use(bodyParser.json()); //for JSON/APPLICATION data format
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -14,4 +18,15 @@ app.use((req, res, next) => {
 //
 app.use("/feed", feedRoute);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+})
+
+mongoose.connect("mongodb+srv://NodeJS:mwIFTGKGpq13p04B@cluster0.gxehntz.mongodb.net/messages").then(() => {
+    app.listen(8080);
+}).catch(err => {
+    console.log(err);
+});
--- controller/feed.js
+++ controller/feed.js
@@ -1,24 +1,63 @@
+const { validationResult } = require("express-validator");
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
-  console.log("form getPost controller");
-  res.status(200).json({
-    posts: [{
-      _id: "1",
-      title: 'My 1st Post!',
-      creator: { name: 'Sam' },
-      createdAt: new Date(),
-      image: "/images/myDuck.jpg",
-      content: 'Hello all, You are Welcome'
-    }]
+  Post.find().then(posts => {
+    if (!posts) {
+      res.status(200).json({ message: "No one posted yet!" });
+    }
+    res.status(200).json({ message: "posts are fatched successfully!", posts: posts })
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
   });
 };
 
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId).then(post => {
+    if (!post) {
+      const error = new Error("Could not find the post!");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ message: "Post fatched", post: post })
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  });
+}
+
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed on the server end!");
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
   //create post in DB
-
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: { id: new Date().toDateString(), title: title, content: content },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/myDuck.jpg",
+    creator: { name: "Samirul" }
+  });
+  post.save().then(result => {
+    console.log(result);
+    res.status(201).json({
+      message: "Post created successfully!",
+      post: result
+    });
+  }).catch(err => {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
   });
 };
